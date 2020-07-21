32a13b6136ba3f2eda2e49bfb4661f58d960f37f
Jozef Reisinger <jozef.reisinger@pan-net.eu>
2020-07-21 17:49:09 +0200

    minor changes in lines/lines_test.go

--- lines/lines_test.go
+++ lines/lines_test.go
@@ -14,8 +14,7 @@ line3`
 
 func TestCountLines(t *testing.T) {
 	input := bytes.NewBufferString(lines)
-	counts := make(map[string]int)
-	counts = Count(input)
+	counts := Count(input)
 	if counts["line1"] != 1 {
 		t.Errorf("count for line1 should be 1 is %d\n", counts["line1"])
 	}
